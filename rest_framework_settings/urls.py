516ac2df488caab61b4d76f010de08889b76e057
Your Name <your.email@example.com>
2024-02-07 11:23:33 +0600

    first commit

---
+++ rest_framework_settings/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+from . views import UserViewSet
+router = DefaultRouter()
+router.register('users', UserViewSet)
+urlpatterns = [
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api/', include('rest_framework.urls')),
+  
+]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
